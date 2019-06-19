e13bfc4071b772af4f1b5beb46c5afef363ea1bf
Anton <anton@adc.sh>
2019-06-20 02:15:57 +0300

    add src + types

--- types/index.js
+++ types/index.js
@@ -1,9 +1,10 @@
+export {}
 /* typal types/index.xml closure noSuppress */
 /**
- * @typedef {_@idio/vscode-jsdoc-1.Config} Config Options for the program.
+ * @typedef {_idio.Config} Config Options for the program.
  */
 /**
- * @typedef {Object} _@idio/vscode-jsdoc-1.Config Options for the program.
+ * @typedef {Object} _idio.Config Options for the program.
  * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
  * @prop {string} text A text to return.
  */
