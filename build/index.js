bf5362333ca847ff074482bcdff3328bd246828e
Anton <anton@adc.sh>
2019-06-20 02:19:19 +0300

    b

--- build/index.js
+++ build/index.js
@@ -1,34 +1,6 @@
-const { debuglog } = require('util');
+export {}
 
-const LOG = debuglog('@idio/vscode-jsdoc-1')
-
-/**
- * The test of jsdoc
- * @param {_@idio/vscode-jsdoc-1.Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
- */
-               async function vscodeJsdoc_1(config = {}) {
-  const {
-    shouldRun = true,
-    text,
-  } = config
-  if (!shouldRun) return
-  LOG('@idio/vscode-jsdoc-1 called with %s', text)
-  return text
-}
-
-/* documentary types/index.xml */
 /**
  * @suppress {nonStandardJsDocs}
- * @typedef {_@idio/vscode-jsdoc-1.Config} Config Options for the program.
+ * @typedef {import('../types').Config} _idio.Config
  */
-/**
- * @suppress {nonStandardJsDocs}
- * @typedef {Object} _@idio/vscode-jsdoc-1.Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
- */
-
-
-module.exports = vscodeJsdoc_1
